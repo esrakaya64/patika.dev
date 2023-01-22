dfabdc997225593cf366bf64cb130d9defb40d87
Esra Kaya <112573124+esrakaya64@users.noreply.github.com>
2023-01-22 18:51:45 +0300

    Big nearest and small nearest number of array

---
+++ NearestNumber/src/nearestnumber/NearestNumber.java
@@ -0,0 +1,54 @@
+
+package nearestnumber;
+import java.util.Scanner;
+/**
+ *
+ * @author Esra Kaya
+ */
+public class NearestNumber {
+
+
+    public static void main(String[] args) {
+       
+        
+        int[] list = {56, 34, 1, 8, 101, -2, -33};
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int a = input.nextInt();
+        input.close();
+
+        int min = list[0];
+        int max = list[0];
+
+        for (int i : list) {
+            if (i < min) {
+                min = i;
+            }
+            if (i > max) {
+                max = i;
+            }
+        }
+
+        System.out.println("Minimum Değer " + min);
+        System.out.println("Maximum Değer " + max);
+        System.out.println("Girilen Sayı: " + a);
+
+        int smallNearest = min;
+        int bigNearest = max;
+
+        for (int i : list) {
+            if (i < a) {
+                smallNearest = (a - smallNearest < a - i) ? smallNearest : i;
+            } else if (i > a) {
+                bigNearest = (bigNearest - a < i - a) ? bigNearest : i;
+            }
+        }
+
+        System.out.println("Small nearest number : " + smallNearest);
+        System.out.println("Big nearest number: " + bigNearest);
+
+        
+
+    }
+    
+
